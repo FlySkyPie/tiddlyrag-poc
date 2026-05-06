5afa72af6f49c61c253da1ec4b569a58c6d93a4c
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2026-05-06 12:35:56 +0800

    feat: expose gitea service to endpoints

--- src/gitea/gitea.module.ts
+++ src/gitea/gitea.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 
 import { GiteaRepository } from './gitea.repository';
+import { GiteaController } from './gitea.controller';
 
 @Module({
   imports: [HttpModule],
+  controllers: [GiteaController],
   providers: [GiteaRepository],
   exports: [GiteaRepository],
 })
